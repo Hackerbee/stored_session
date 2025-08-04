78bfa3d7ff620222f08946970668afa2b7dcf4ae
Akash Patel <akash29697@gmail.com>
2025-08-04 16:11:03 +0530

    (feat) Support config/stored_session.yml configuation file (fix) solved ActiveModel::ForbiddenAttributesError raised on StoredSession::Configuration class initialization

--- lib/stored_session/engine.rb
+++ lib/stored_session/engine.rb
@@ -13,7 +13,24 @@ module StoredSession
     end
 
     initializer "stored_session.config" do |app|
-      StoredSession.config = StoredSession::Configuration.new(app.config.stored_session)
+      app.paths.add "config/stored_session", with: "config/stored_session.yml"
+      config_exists = Pathname.new(app.config.paths["config/stored_session"].first).exist?
+      options = config_exists ? app.config_for(:stored_session).to_h.deep_symbolize_keys : {}
+
+      options[:connects_to] = config.stored_session.connects_to if config.stored_session.connects_to
+
+      options[:base_controller_class_name] = config.stored_session.base_controller_class_name if config.stored_session.base_controller_class_name
+      options[:base_job_class_name] = config.stored_session.base_job_class_name if config.stored_session.base_job_class_name
+      options[:base_record_class_name] = config.stored_session.base_record_class_name if config.stored_session.base_record_class_name
+
+      options[:sessions_table_name] = config.stored_session.sessions_table_name if config.stored_session.sessions_table_name
+      options[:session_class_name] = config.stored_session.session_class_name if config.stored_session.session_class_name
+      options[:session_max_created_age] = config.stored_session.session_max_created_age if config.stored_session.session_max_created_age
+      options[:session_max_updated_age] = config.stored_session.session_max_updated_age if config.stored_session.session_max_updated_age
+
+      options[:expire_sessions_job_queue_as] = config.stored_session.expire_sessions_job_queue_as if config.stored_session.expire_sessions_job_queue_as
+
+      StoredSession.config = StoredSession::Configuration.new(**options)
     end
 
     initializer "stored_session.logger" do
